d66e2cb3abdeb3e094b76ecf91c90ccdfc74d15f
Griffin P Longtin <griflongtin@live.com>
2018-01-19 14:22:16 -0800

    add the done function to the editAnimal

--- src/app/edit-animal/edit-animal.component.ts
+++ src/app/edit-animal/edit-animal.component.ts
@@ -8,7 +8,12 @@ import { Animal } from '../animal.model';
 export class EditAnimalComponent implements OnInit {
 
   @Input() selectedEditAnimal: Animal[];
+  @Output() doneclickAnimalEditbutton = new EventEmitter();
 
+
+  doneButtonClicked() {
+    this.doneclickAnimalEditbutton.emit();
+  }
   constructor() { }
 
   ngOnInit() {
