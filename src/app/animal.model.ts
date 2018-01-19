67cd4526e38c69b8fbb8c7203ce96978b8d7f7ce
Griffin P Longtin <griflongtin@live.com>
2018-01-19 10:24:19 -0800

    add animal model to build OBJs

---
+++ src/app/animal.model.ts
@@ -0,0 +1,13 @@
+export class Animal {
+  Species: string;
+  Name: string;
+  Age: number;
+  Diet: string;
+  Location: string;
+  Caretakers: number;
+  Sex: string;
+  Likes: string;
+  Dislikes: string;
+  // ThumbesUp: number;
+  // Thumbesdown: number;
+}
